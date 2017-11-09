af9b66a8dd9c3bfe0f772256f866ab5b4d414d21
Sebastian Meier <contact@sebastianmeier.eu>
2017-11-09 18:31:34 +0100

    postinstall

--- postinstall.js
+++ postinstall.js
@@ -2,7 +2,7 @@ var imports = require("google-font-import");
  
 var opts = {
   src: './http/template/vis.html',
-  htmlpath: './http/vis.html',
+  htmlpath: './http',
   fontpath: './http/assets/fonts',
   stylepath: './http/assets/style'
 };
