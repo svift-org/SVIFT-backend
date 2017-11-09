5a6c645e87a610896a0f798e42f97fbd35344ac8
Sebastian Meier <contact@sebastianmeier.eu>
2017-11-09 22:52:52 +0100

    postinstall fonts

--- postinstall.js
+++ postinstall.js
@@ -14,6 +14,24 @@ var opts = {
  
 imports(opts).then(function () {
 
-	console.log('google fonts download')
+	fs.readdirSync('./http/assets/style').forEach(file => {
+      if(file.indexOf('css_family') > -1){
+        let css = fs.readFileSync('./http/assets/style/'+file, 'utf8'),
+        	vis = fs.readFileSync('./http/vis.html', 'utf8'),
+        	v1 = vis.indexOf('<link rel="stylesheet" href="assets/style/css_family'),
+        	v2 = vis.indexOf('>', v1)
+
+        css = css.split('../fonts').join('./assets/fonts')
+
+        let nvis = vis.substr(0,v1)+'<style type="text/css">'+css+'</style>'+vis.substr(v2+1)
+
+        fs.writeFileSync('./http/vis.html', nvis, 'utf8')
+
+        vis.substr()
+		        
+      }
+    })
+
+	console.log('postinstall complete')
 
 });
