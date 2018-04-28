491c2f293db56f17c71cb36f258dd979fc5aca8b
Sebastian Meier <contact@sebastianmeier.eu>
2018-04-28 11:03:27 +0200

    style fixing

--- postinstall.js
+++ postinstall.js
@@ -1,23 +1,23 @@
 var imports = require("google-font-import"),
 	fs = require('fs')
 
-if (!fs.existsSync('./http/assets')) {
-  fs.mkdirSync('./http/assets')
+if (!fs.existsSync(__dirname + '/http/assets')) {
+  fs.mkdirSync(__dirname + '/http/assets')
 }
 
 var opts = {
-  src: './http/template/vis.html',
-  htmlpath: './http',
-  fontpath: './http/assets/fonts',
-  stylepath: './http/assets/style'
+  src: __dirname + '/http/template/vis.html',
+  htmlpath: __dirname + '/http',
+  fontpath: __dirname + '/http/assets/fonts',
+  stylepath: __dirname + '/http/assets/style'
 };
  
 imports(opts).then(function () {
 
-	fs.readdirSync('./http/assets/style').forEach(file => {
+	fs.readdirSync(__dirname + '/http/assets/style').forEach(file => {
       if(file.indexOf('css_family') > -1){
-        let css = fs.readFileSync('./http/assets/style/'+file, 'utf8'),
-        	vis = fs.readFileSync('./http/vis.html', 'utf8'),
+        let css = fs.readFileSync(__dirname + '/http/assets/style/'+file, 'utf8'),
+        	vis = fs.readFileSync(__dirname + '/http/vis.html', 'utf8'),
         	v1 = vis.indexOf('<link rel="stylesheet" href="assets/style/css_family'),
         	v2 = vis.indexOf('>', v1)
 
@@ -25,9 +25,9 @@ imports(opts).then(function () {
 
         let nvis = vis.substr(0,v1)+'<style type="text/css">'+css+'</style>'+vis.substr(v2+1)
 
-        fs.writeFileSync('./http/vis.html', nvis, 'utf8')
+        fs.writeFileSync(__dirname + '/http/vis.html', nvis, 'utf8')
 
-        
+
 		        
       }
     })
